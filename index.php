216de448eb28277e5553f6c54ece25b7bf01d0aa
ssanchez273 <ssanchez@limadigital.pe>
2022-10-25 21:14:23 -0500

    Section Nuestros Valores

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
 <?php
-include_once(__DIR__ . "/components/head.php") ?>
+include_once(__DIR__ . "/components/head.php"); ?>
 <body>
-<?php include_once(__DIR__ . "/components/nav.php") ?>
+<?php include_once(__DIR__ . "/components/nav.php"); ?>
   <main>
- 
+  <?php include_once("pages/home/home.php"); ?>
   </main>
   
 
